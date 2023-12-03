b3e47d94b53aadff1dc0caae8b49052f9054a28d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-03 15:37:22 +0900

    Extract Menu class

--- mygame/app/menu.rb
+++ mygame/app/menu.rb
@@ -1,35 +1,39 @@
 ALL_DAY_NUMBERS = (1..2).to_a.freeze
 
+class Menu
+  def initialize(args)
+    ui = args.state.ui
+
+    @buttons = []
+    ALL_DAY_NUMBERS.each do |day_number|
+      ui.buttons[day_number] = {
+        w: 200, h: 40,
+        text: "Day #{day_number}",
+        scene: ('day%02d' % day_number).to_sym
+      }
+      @buttons << ui.buttons[day_number]
+    end
+
+    arrange_in_two_columns(@buttons)
+  end
+
+  def tick(args)
+    clicked_button = @buttons.find { |button| button[:clicked] }
+    go_to_scene(args, clicked_button[:scene]) if clicked_button
+
+    args.outputs.primitives << [
+      headline('Advent Of Code 2023'),
+      button_primitives(@buttons)
+    ]
+  end
+end
+
 def menu_tick(args)
   menu_setup(args) if args.state.scene_tick.zero?
 
-  buttons = menu_day_buttons(args)
-
-  clicked_button = buttons.find { |button| button[:clicked] }
-  go_to_scene(args, clicked_button[:scene]) if clicked_button
-
-  args.outputs.primitives << [
-    headline('Advent Of Code 2023'),
-    button_primitives(buttons)
-  ]
+  args.state.screen.tick(args)
 end
 
 def menu_setup(args)
-  buttons = args.state.ui.buttons
-  ALL_DAY_NUMBERS.each do |day_number|
-    buttons[day_number] = {
-      w: 200, h: 40,
-      text: "Day #{day_number}",
-      scene: ('day%02d' % day_number).to_sym
-    }
-  end
-
-  arrange_in_two_columns(
-    menu_day_buttons(args)
-  )
-end
-
-def menu_day_buttons(args)
-  buttons = args.state.ui.buttons
-  ALL_DAY_NUMBERS.map { |day_number| buttons[day_number] }
+  args.state.screen = Menu.new(args)
 end
