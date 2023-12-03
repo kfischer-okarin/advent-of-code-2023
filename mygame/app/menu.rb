ed355c8085c03c038ec961c94aabef1080889119
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-03 19:03:55 +0900

    Automatically generate Menu buttons

--- mygame/app/menu.rb
+++ mygame/app/menu.rb
@@ -1,11 +1,9 @@
-ALL_DAY_NUMBERS = (1..2).to_a.freeze
-
 class Menu
   def initialize(args)
     ui = args.state.ui
 
     @buttons = []
-    ALL_DAY_NUMBERS.each do |day_number|
+    available_day_numbers.each do |day_number|
       ui.buttons[day_number] = button(
         w: 200, h: 40,
         text: "Day #{day_number}",
@@ -28,5 +26,17 @@ class Menu
       headline('Advent Of Code 2023'),
       button_primitives(@buttons)
     ]
+  end
+
+  private
+
+  def available_day_numbers
+    result = []
+    (1..25).each do |day_number|
+      break unless Object.const_defined?('Day%02d' % day_number)
+
+      result << day_number
+    end
+    result
   end
 end
