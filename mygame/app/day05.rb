a465d2e57089589813b7c06c9d74a397a71c7d89
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-05 20:36:34 +0900

    Day 5 - Part 2

--- mygame/app/day05.rb
+++ mygame/app/day05.rb
@@ -2,17 +2,24 @@ module Day05
   class << self
     def result(part)
       parts = read_inputs(5).split("\n\n")
-      seeds = parse_seeds(parts.shift)
+      seeds_line = parts.shift
       maps = parts.map { |mapping_string| parse_mappings(mapping_string) }
 
       case part
       when 1
+        seeds = parse_seeds(seeds_line)
         result = seeds
         maps.each do |map|
           result = result.map { |number| map_number(map, number) }
         end
         result.min
       when 2
+        seed_ranges = parse_seed_ranges(seeds_line)
+        result = seed_ranges
+        maps.each do |map|
+          result = result.flat_map { |range| map_range(map, range) }
+        end
+        result.min_by(&:begin).begin
       end
     end
 
@@ -38,6 +45,74 @@ module Day05
       return number unless applying_mapping
 
       number + applying_mapping[:offset]
+    end
+
+    def parse_seed_ranges(seed_line)
+      seeds = parse_seeds(seed_line)
+      seeds.each_slice(2).map { |start, length| (start...(start + length)) }
+    end
+
+    def check_overlap(range, overlapping_range)
+      begins_before = range.begin < overlapping_range.begin
+      ends_before = range.end <= overlapping_range.begin
+      begins_after = range.begin >= overlapping_range.end
+      ends_after = range.end > overlapping_range.end
+
+      if (begins_before && ends_before) || (begins_after && ends_after)
+        return {
+          overlapping: nil,
+          non_overlapping: [range]
+        }
+      end
+
+      if (!begins_before && !ends_after)
+        return {
+          overlapping: range,
+          non_overlapping: []
+        }
+      end
+
+      if (begins_before && ends_after)
+        return {
+          overlapping: overlapping_range,
+          non_overlapping: [(range.begin...overlapping_range.begin), (overlapping_range.end...range.end)]
+        }
+      end
+
+      if begins_before
+        {
+          overlapping: (overlapping_range.begin...range.end),
+          non_overlapping: [(range.begin...overlapping_range.begin)]
+        }
+      else
+        {
+          overlapping: (range.begin...overlapping_range.end),
+          non_overlapping: [(overlapping_range.end...range.end)]
+        }
+      end
+    end
+
+    def map_range(mappings, range)
+      unmapped_ranges = [range]
+      result = []
+      until unmapped_ranges.empty?
+        unmapped_range = unmapped_ranges.shift
+        overlap = nil
+        applicable_mapping = mappings.find { |mapping|
+          overlap = check_overlap(unmapped_range, mapping[:from_range])
+          overlap[:overlapping]
+        }
+
+        if applicable_mapping
+          range = overlap[:overlapping]
+          offset = applicable_mapping[:offset]
+          result << ((range.begin + offset)...(range.end + offset))
+          unmapped_ranges.concat overlap[:non_overlapping]
+        else
+          result << unmapped_range
+        end
+      end
+      result
     end
   end
 end
--- mygame/tests/app/day05.rb
+++ mygame/tests/app/day05.rb
@@ -29,3 +29,81 @@ def test_day05_map_number(_args, assert)
   assert.equal! Day05.map_number(mappings, 13), 13
   assert.equal! Day05.map_number(mappings, 98), 50
 end
+
+def test_day05_parse_seed_ranges(_args, assert)
+  assert.equal! Day05.parse_seed_ranges('seeds: 79 14 55 13'), [(79...93), (55...68)]
+end
+
+def test_day05_check_overlap(_args, assert)
+  [
+    {
+      range: (1...10),
+      overlapping_range: (7...20),
+      result: {
+        overlapping: (7...10),
+        non_overlapping: [(1...7)]
+      }
+    },
+    {
+      range: (10...20),
+      overlapping_range: (5...15),
+      result: {
+        overlapping: (10...15),
+        non_overlapping: [(15...20)]
+      }
+    },
+    {
+      range: (10...20),
+      overlapping_range: (5...25),
+      result: {
+        overlapping: (10...20),
+        non_overlapping: []
+      }
+    },
+    {
+      range: (10...20),
+      overlapping_range: (13...17),
+      result: {
+        overlapping: (13...17),
+        non_overlapping: [(10...13), (17...20)]
+      }
+    },
+    {
+      range: (10...20),
+      overlapping_range: (30...40),
+      result: {
+        overlapping: nil,
+        non_overlapping: [(10...20)]
+      }
+    },
+    {
+      range: (10...20),
+      overlapping_range: (0...3),
+      result: {
+        overlapping: nil,
+        non_overlapping: [(10...20)]
+      }
+    }
+  ].each do |test_case|
+    assert.equal! Day05.check_overlap(test_case[:range], test_case[:overlapping_range]),
+                  test_case[:result]
+  end
+end
+
+def test_day05_map_range(_args, assert)
+  mappings = [
+    { from_range: (98...100), offset: -48 },
+    { from_range: (50...98), offset: 2 }
+  ]
+
+  [
+    { range: (79...93), result: [(81...95)] },
+    { range: (40...60), result: [(40...50), (52...62)] },
+    { range: (99...105), result: [(51...52), (100...105)] },
+    { range: (80...120), result: [(50...52), (82...100), (100...120)] }
+  ].each do |test_case|
+    result = Day05.map_range(mappings, test_case[:range])
+    result = result.sort_by(&:begin)
+    assert.equal! result, test_case[:result]
+  end
+end
