abd3259f1b8770bb8c72431982b2ceeed9357384
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-12-15 20:02:28 +0900

    Day 15 - Part 2

--- mygame/app/day15.rb
+++ mygame/app/day15.rb
@@ -6,6 +6,12 @@ module Day15
       case part
       when 1
         instruction_hash_sum(instructions)
+      when 2
+        boxes = {}
+        instructions.each do |instruction|
+          execute_instruction(instruction, boxes)
+        end
+        total_focusing_power(boxes)
       end
     end
 
@@ -25,6 +31,38 @@ module Day15
 
     def instruction_hash_sum(instructions)
       instructions.sum { |instruction| hash_value(instruction) }
+    end
+
+    def execute_instruction(instruction, boxes)
+      if instruction.end_with?('-')
+        label = instruction[0..-2]
+        box_number = hash_value(label)
+        box = boxes[box_number]
+        return unless box
+
+        box.reject! { |lens| lens[0] == label.to_sym }
+        boxes.delete box_number if box.empty?
+      else
+        label, focal_length_string = instruction.split('=')
+        box_number = hash_value(label)
+        box = boxes[box_number] ||= []
+        lens_to_insert = [label.to_sym, focal_length_string.to_i]
+        existing_lens_index = box.find_index { |lens| lens[0] == lens_to_insert[0] }
+        if existing_lens_index
+          box[existing_lens_index] = lens_to_insert
+        else
+          box << lens_to_insert
+        end
+      end
+    end
+
+    def total_focusing_power(boxes)
+      boxes.keys.sum { |box_number|
+        box = boxes[box_number]
+        box.each_with_index.to_a.sum { |lens, index|
+          (box_number + 1) * (index + 1) * lens[1]
+        }
+      }
     end
   end
 end
--- mygame/tests/app/day15.rb
+++ mygame/tests/app/day15.rb
@@ -14,3 +14,46 @@ def test_day15_instruction_hash_sum(_args, assert)
   instructions = ['rn=1', 'cm-', 'qp=3', 'cm=2', 'qp-', 'pc=4', 'ot=9', 'ab=5', 'pc-', 'pc=6', 'ot=7']
   assert.equal! Day15.instruction_hash_sum(instructions), 1320
 end
+
+def test_day15_execute_instruction(_args, assert)
+  boxes = {}
+
+  Day15.execute_instruction('rn=1', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1]] }
+
+  Day15.execute_instruction('cm-', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1]] }
+
+  Day15.execute_instruction('qp=3', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1]], 1 => [[:qp, 3]] }
+
+  Day15.execute_instruction('cm=2', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 1 => [[:qp, 3]] }
+
+  Day15.execute_instruction('qp-', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]] }
+
+  Day15.execute_instruction('pc=4', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:pc, 4]] }
+
+  Day15.execute_instruction('ot=9', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:pc, 4], [:ot, 9]] }
+
+  Day15.execute_instruction('ab=5', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:pc, 4], [:ot, 9], [:ab, 5]] }
+
+  Day15.execute_instruction('pc-', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:ot, 9], [:ab, 5]] }
+
+  Day15.execute_instruction('pc=6', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:ot, 9], [:ab, 5], [:pc, 6]] }
+
+  Day15.execute_instruction('ot=7', boxes)
+  assert.equal! boxes, { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:ot, 7], [:ab, 5], [:pc, 6]] }
+end
+
+def test_day15_total_focusing_power(_args, assert)
+  boxes = { 0 => [[:rn, 1], [:cm, 2]], 3 => [[:ot, 7], [:ab, 5], [:pc, 6]] }
+
+  assert.equal! Day15.total_focusing_power(boxes), 145
+end
